28268075a08c35e2ebd91d21ab0e1a9f5eb9be54
dcastellini <dcastellini89@gmail.com>
2016-02-22 21:46:55 -0300

    cambios damian

--- DoFit/aplication/protected/config/main.php
+++ DoFit/aplication/protected/config/main.php
@@ -9,7 +9,7 @@ return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
 	'name'=>'Do Fit',
 
-    'defaultController' => 'Site/login',
+    'defaultController' => 'Site/Selector',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
---
+++ DoFit/aplication/protected/views/site/Selector.php
@@ -0,0 +1,40 @@
+<link href="<?php echo Yii::app()->request->baseUrl; ?>/css/coverselector.css" rel="stylesheet">
+
+
+<div class="site-wrapper">
+    <div class="site-wrapper-inner">
+        <div class="cover-container">
+            <div class="masthead clearfix">
+                <div class="inner">
+                    <h2 class="masthead-brand"><img class="navbar-brand-img" src="<?php echo Yii::app()->request->baseUrl; ?>/img/logo_blanco.png" alt="First slide"></h2>
+                </div>
+            </div>
+            <div class="inner cover">
+                <h1 class="cover-heading">Identificate, ¿quien sos?</h1>
+                <p class="lead">
+                        <div class="form-group">
+                            <a href="login" class='btn btn-primary btn-block' id='boton'>Un alumno</a>
+                        </div>
+                        <div class="form-group">
+                            <a href="login" class='btn btn-primary btn-block' id='boton'>Un profesor</a>
+                        </div>
+                        <div class="form-group">
+                            <a href="logininstitucion" class='btn btn-primary btn-block' id='boton'>Un gimnasio</a>
+                        </div>
+                        <div class="form-group">
+                            <a href="loginadmin" class='btn btn-primary btn-block' id='boton'>Un admin</a>
+                        </div>
+                </a></p>
+            </div>
+            <div class="mastfoot">
+                <div class="inner">
+                    <p>DoFit!</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
